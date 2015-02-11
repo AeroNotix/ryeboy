239dacc35d7d5b304f329d29b2dad7911e8a7a31
Aaron France <aaron.l.france@gmail.com>
2015-02-12 00:35:27 +0100

    Implement unix time funsies

---
+++ time.lisp
@@ -0,0 +1,14 @@
+(in-package :ryeboy)
+
+
+(defvar *unix-epoch-difference*
+  (encode-universal-time 0 0 0 1 1 1970 0))
+
+(defun universal-to-unix-time (universal-time)
+  (- universal-time *unix-epoch-difference*))
+
+(defun unix-to-universal-time (unix-time)
+  (+ unix-time *unix-epoch-difference*))
+
+(defun get-unix-time ()
+  (universal-to-unix-time (get-universal-time)))
