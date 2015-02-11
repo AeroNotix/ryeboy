f5080a10b47559a020187178eda5ba628901b477
Aaron France <aaron.l.france@gmail.com>
2015-02-12 00:35:19 +0100

    Improve send-event

--- ryeboy-conn.lisp
+++ ryeboy-conn.lisp
@@ -7,3 +7,11 @@
                            :element-type '(unsigned-byte 8)
                            :protocol (if (eq :tcp connection-type)
                                          :stream :datagram))))
+
+(defun send-event (conn event)
+  ;; TODO: UDP connections aren't length prefixed, namsay
+  (let ((encoded (thing->bytes event)))
+    (write-int (length encoded) conn)
+    (write-sequence (thing->bytes event) conn)
+    (finish-output conn)
+    (values)))
