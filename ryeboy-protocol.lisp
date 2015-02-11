9197d94347d02769ce51be7190a41791c17e4ce2
Aaron France <aaron.l.france@gmail.com>
2015-02-12 00:35:03 +0100

    Reorganise code to allow code to, you know, compile

--- ryeboy-protocol.lisp
+++ ryeboy-protocol.lisp
@@ -1,33 +1,6 @@
 (in-package :ryeboy)
 
 
-(defun make-event (&key
-                     (time (encode-universal-time 0 0 0 1 1 1970 0))
-                     (host (machine-instance))
-                     state service description tags ttl attrs metric)
-  (let ((event (make-instance 'com.aphyr.riemann:event)))
-    (setf (com.aphyr.riemann:time event) time)
-    (setf (com.aphyr.riemann:host event) (protocol-buffer:string-field host))
-    (when state
-      (setf (com.aphyr.riemann:state event) (protocol-buffer:string-field state)))
-    (when service
-      (setf (com.aphyr.riemann:service event) (protocol-buffer:string-field service)))
-    (when description
-      (setf (com.aphyr.riemann:description event) (protocol-buffer:string-field description)))
-    (when tags
-      (setf (com.aphyr.riemann:tags event)
-            (map 'vector #'protocol-buffer:string-field tags)))
-    (when ttl
-      (setf (com.aphyr.riemann:ttl event) (float ttl)))
-    (when attrs
-      (setf (com.aphyr.riemann:attributes event) attrs))
-    (when metric
-      ;; TODO:
-      ;;   Aphyr's library does. Ask him why.
-      (set-metric event (float metric))
-      (set-metric event metric))
-    event))
-
 (defgeneric set-metric (event metric)
   (:documentation "Set the metric type on the event"))
 
@@ -53,3 +26,30 @@
   (let ((e (make-instance 'com.aphyr.riemann:event)))
     (pb:merge-from-array e bytes 0 (length bytes))
     e))
+
+(defun make-event (&key
+                     (time (get-unix-time))
+                     (host (machine-instance))
+                     state service description tags ttl attrs metric)
+  (let ((event (make-instance 'com.aphyr.riemann:event)))
+    (setf (com.aphyr.riemann:time event) time)
+    (setf (com.aphyr.riemann:host event) (protocol-buffer:string-field host))
+    (when state
+      (setf (com.aphyr.riemann:state event) (protocol-buffer:string-field state)))
+    (when service
+      (setf (com.aphyr.riemann:service event) (protocol-buffer:string-field service)))
+    (when description
+      (setf (com.aphyr.riemann:description event) (protocol-buffer:string-field description)))
+    (when tags
+      (setf (com.aphyr.riemann:tags event)
+            (map 'vector #'protocol-buffer:string-field tags)))
+    (when ttl
+      (setf (com.aphyr.riemann:ttl event) (float ttl)))
+    (when attrs
+      (setf (com.aphyr.riemann:attributes event) attrs))
+    (when metric
+      ;; TODO:
+      ;;   Aphyr's library does. Ask him why.
+      (set-metric event (float metric))
+      (set-metric event metric))
+    event))
